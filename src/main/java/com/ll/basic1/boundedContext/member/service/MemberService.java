794d71f1bf2f264497e5860890bbca2a1be81ada
Choi-Hyeok99 <gur0709@naver.com>
2023-03-15 15:18:02 +0900

    data에 Member 객체 리모콘 담기

--- src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/basic1/boundedContext/member/service/MemberService.java
@@ -20,7 +20,7 @@ public class MemberService {
         }
 
 
-        return RsData.of("S-1", "%s 님 환영합니다.".formatted(username), member.getId());
+        return RsData.of("S-1", "%s 님 환영합니다.".formatted(username), member);
     }
 
     public Member findByUsername(String username) {
