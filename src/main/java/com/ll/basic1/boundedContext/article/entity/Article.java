fffba5f857e0240ad7d1c208821585c5e93d82b3
Choi-Hyeok99 <gur0709@naver.com>
2023-03-17 15:35:09 +0900

    등록날짜,수정날짜 자동갱신

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -2,12 +2,15 @@ package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-
+import jakarta.persistence.EntityListeners;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -16,13 +19,16 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
 
     @Id
     @GeneratedValue(strategy = IDENTITY) //AUTO_INCREMENT
     private long id;
+    @CreatedDate
 
     private LocalDateTime createDate; // 데이터 생성 날짜
+    @LastModifiedDate
     private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
     private String body;
