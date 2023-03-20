0b66e9dc2c1665162ef7578766968f4fca3ec909
Choi-Hyeok99 <gur0709@naver.com>
2023-03-20 14:23:00 +0900

    오류

--- Spring/jumpSpring/sbb/src/main/java/com/mysite/sbb/MainController.java
+++ Spring/jumpSpring/sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller // 써야함
 public class MainController {
     @GetMapping("/sbb")
-    @ResponseBody // 이걸 그대로 보여주겠다 (안쓰면 사이트에 안뜸)
+    @ResponseBody
     public String index(){
-        return "안녕하세요 sbb에 오신것을 환영합니다.";
+        return "안녕하세요 sbb에 오신것을 환영합니다.<br>하하";
     }
 
 }
