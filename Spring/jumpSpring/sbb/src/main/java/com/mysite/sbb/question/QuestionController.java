3d00deac9f45f08cd291487c4b9ec4ca646cc2cf
Choi-Hyeok99 <gur0709@naver.com>
2023-03-23 13:39:11 +0900

    QuestionForm 바인딩 에러 발생시 폼 html 으로 응답

--- Spring/jumpSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ Spring/jumpSpring/sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,14 +3,20 @@ package com.mysite.sbb.question;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/question")
 @RequiredArgsConstructor
+// @Validated 컨트롤러에서는 이 부분 생략가능
 public class QuestionController {
     private final QuestionService questionService;
 
@@ -21,8 +27,8 @@ public class QuestionController {
         model.addAttribute("questionList", questionList);
 
         return "question_list";
-
     }
+
     @GetMapping("/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
         Question question = questionService.getQuestion(id);
@@ -30,17 +36,25 @@ public class QuestionController {
         model.addAttribute("question", question);
 
         return "question_detail";
-}
+    }
+
     @GetMapping("/create")
     public String questionCreate() {
         return "question_form";
     }
-    @PostMapping("/create")
-    public String questionCreate(@Valid QuestionForm questionForm) {
-        String subject = questionForm.getSubject();
-        String content = questionForm.getContent();
 
-        questionService.create(subject, content);
+    @PostMapping("/create")
+    // @Valid QuestionForm questionForm
+    // questionForm 값을 바인딩 할 때 유효성 체크를 해라!
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if ( bindingResult.hasErrors() ) {
+            // question_form.html 실행
+            // 다시 작성하라는 의미로 응답에 폼을 실어서 보냄
+            return "question_form";
+        }
+
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
+
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
