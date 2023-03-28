07316be4f12d3747737ec442923c3c3916db6b46
Choi-Hyeok99 <gur0709@naver.com>
2023-03-28 14:07:39 +0900

    82까지

--- Spring/jumpSpring/sbb/src/main/java/com/mysite/sbb/question/Question.java
+++ Spring/jumpSpring/sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -3,13 +3,14 @@ package com.mysite.sbb.question;
 import com.mysite.sbb.answer.Answer;
 import jakarta.persistence.*;
 import lombok.Getter;
-import java.util.List;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.ArrayList;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 // question 테이블이 생김
 @Getter
@@ -17,8 +18,6 @@ import java.time.LocalDateTime;
 @Entity
 @ToString
 public class Question {
-
-
     @Id // PRIMARY KEY
     @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
     private Integer id; // INT id
@@ -31,22 +30,19 @@ public class Question {
 
     private LocalDateTime createDate; // DATETIME
 
-
     // OneToMany 자바세상에서의 편의를 위해서 필드 생성
-    // 이 녀석은 실제 DB에 칼럼이 생성되지 않는다. (반영 X)
+    // 이 녀석은 실제 DB 테이블에 칼럼이 생성되지 않는다.
     // DB는 배열이나 리스트를 저장할 수 없다.
-        // 칼럼에 저장할 수  없다.
+    // 칼럼에 저장할 수 없다.
     // 만들어도 되고 안 만들어도 됩니다.
     // 다만 만들면 해당 객체(질문객체)에서 관련된 답변들을 찾을 때 편합니다.
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    @LazyCollection(LazyCollectionOption.EXTRA) // answerList.size(); 함수가 실행될 때 SELECT COUNT 실행
     // OneToMany 에는 직접객체초기화
-    private List<Answer> answerList = new ArrayList<>(); // OneToMany에는 무조건  new ArrayList<>() 이거 무조건 써라
+    private List<Answer> answerList = new ArrayList<>();
 
     public void addAnswer(Answer a) {
         a.setQuestion(this);
         answerList.add(a);
     }
-
-
-
 }
--- Spring/jumpSpring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ Spring/jumpSpring/sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,16 +1,18 @@
 package com.mysite.sbb;
 
-
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
 import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.UserRepository;
+import com.mysite.sbb.user.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -20,24 +22,42 @@ import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SbbApplicationTests {
+    @Autowired
+    private QuestionService questionService;
+    @Autowired
+    private UserService userService;
+
     @Autowired
     private QuestionRepository questionRepository;
     @Autowired
     private AnswerRepository answerRepository;
     @Autowired
-    private QuestionService questionService;
+    private UserRepository userRepository;
+
     @BeforeEach
         // 아래 메서드는 각 테스트케이스가 실행되기 전에 실행된다.
     void beforeEach() {
         // 모든 데이터 삭제
         answerRepository.deleteAll();
         answerRepository.clearAutoIncrement();
+
         // 모든 데이터 삭제
         questionRepository.deleteAll();
+
         // 흔적삭제(다음번 INSERT 때 id가 1번으로 설정되도록)
         questionRepository.clearAutoIncrement();
+
+        // 모든 데이터 삭제
+        userRepository.deleteAll();
+        userRepository.clearAutoIncrement();
+
+        // 회원 2명 생성
+        userService.create("user1", "user1@test.com", "1234");
+        userService.create("user2", "user2@test.com", "1234");
+
         // 질문 1개 생성
         Question q1 = new Question();
         q1.setSubject("sbb가 무엇인가요?");
@@ -59,6 +79,7 @@ class SbbApplicationTests {
         a1.setCreateDate(LocalDateTime.now());
         answerRepository.save(a1);
     }
+
     @Test
     @DisplayName("데이터 저장")
     void t001() {
@@ -68,8 +89,10 @@ class SbbApplicationTests {
         q.setContent("알고 싶습니다.");
         q.setCreateDate(LocalDateTime.now());
         questionRepository.save(q);
+
         assertEquals("세계에서 가장 부유한 국가가 어디인가요?", questionRepository.findById(3).get().getSubject());
     }
+
     /*
     SQL
     SELECT * FROM question
@@ -79,9 +102,11 @@ class SbbApplicationTests {
     void t002() {
         List<Question> all = questionRepository.findAll();
         assertEquals(2, all.size());
+
         Question q = all.get(0);
         assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
+
     /*
     SQL
     SELECT *
@@ -92,11 +117,13 @@ class SbbApplicationTests {
     @DisplayName("findById")
     void t003() {
         Optional<Question> oq = questionRepository.findById(1);
+
         if (oq.isPresent()) {
             Question q = oq.get();
             assertEquals("sbb가 무엇인가요?", q.getSubject());
         }
     }
+
     /*
     SQL
     SELECT *
@@ -109,6 +136,7 @@ class SbbApplicationTests {
         Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
         assertEquals(1, q.getId());
     }
+
     /*
     SQL
     SELECT *
@@ -124,6 +152,7 @@ class SbbApplicationTests {
         );
         assertEquals(1, q.getId());
     }
+
     /*
     SQL
     SELECT *
@@ -137,6 +166,7 @@ class SbbApplicationTests {
         Question q = qList.get(0);
         assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
+
     /*
     SQL
     UPDATE
@@ -157,6 +187,7 @@ class SbbApplicationTests {
         q.setSubject("수정된 제목");
         questionRepository.save(q);
     }
+
     /*
     SQL
     DELETE
@@ -177,27 +208,32 @@ class SbbApplicationTests {
         questionRepository.delete(q);
         assertEquals(1, questionRepository.count());
     }
+
     @Test
     @DisplayName("답변 데이터 생성 후 저장하기")
     void t009() {
         Optional<Question> oq = questionRepository.findById(2);
         assertTrue(oq.isPresent());
         Question q = oq.get();
+
         /*
         // v1
         Optional<Question> oq = questionRepository.findById(2);
         Question q = oq.get();
         */
+
         /*
         // v2
         Question q = questionRepository.findById(2).get();
         */
+
         Answer a = new Answer();
         a.setContent("네 자동으로 생성됩니다.");
         a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
         a.setCreateDate(LocalDateTime.now());
         answerRepository.save(a);
     }
+
     @Test
     @DisplayName("답변 조회하기")
     void t010() {
@@ -207,7 +243,8 @@ class SbbApplicationTests {
         assertEquals(2, a.getQuestion().getId());
     }
 
-    @Transactional
+    @Transactional // 여기서의 트랜잭션의 역할 : 함수가 끝날 때까지 전화(DB와의)를 끊지 않음
+    @Rollback(false)
     @Test
     @DisplayName("질문에 달린 답변 찾기")
     void t011() {
@@ -220,6 +257,7 @@ class SbbApplicationTests {
         assertEquals(1, answerList.size());
         assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
+
     @Test
     @DisplayName("대량 테스트 데이터 만들기")
     void t012() {
