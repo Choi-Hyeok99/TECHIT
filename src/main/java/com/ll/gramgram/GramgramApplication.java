654a63acd52b9e482b182b041c982abf48c8ac7c
Choi-Hyeok99 <gur0709@naver.com>
2023-03-30 13:35:39 +0900

    회원가입 처리

--- src/main/java/com/ll/gramgram/GramgramApplication.java
+++ src/main/java/com/ll/gramgram/GramgramApplication.java
@@ -2,11 +2,14 @@ package com.ll.gramgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GramgramApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GramgramApplication.class, args);
     }
 
