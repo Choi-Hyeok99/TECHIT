934bdce87ba5765041aeb859cd5e9b55cc2336ec
Choi-Hyeok99 <gur0709@naver.com>
2023-04-12 13:26:40 +0900

    2Week 수정,likeablePersonServiceTests 생성

---
+++ src/test/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonServiceTests.java
@@ -0,0 +1,32 @@
+package com.ll.gramgram.boundedContext.likeablePerson.service;
+
+
+
+import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+public class LikeablePersonServiceTests {
+    @Autowired
+    private LikeablePersonService likeablePersonService;
+
+    @Test
+    @DisplayName("등록 폼(인스타 인증을 안해서 폼 대신 메세지)")
+    @WithUserDetails("user1")
+    void t001() throws Exception {
+        LikeablePerson likeablePersonId2 = likeablePersonService.findById(2L).get();
+        InstaMember instaMemberUser3    = likeablePersonId2.getFromInstaMember();
+
+        System.out.println(instaMemberUser3);
+    }
+}
