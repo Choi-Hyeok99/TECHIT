36680b00ace42ae97937bb00b22c83235235bca0
Choi-Hyeok99 <gur0709@naver.com>
2023-05-03 14:15:06 +0900

    혼자하는 세팅

---
+++ src/main/java/com/ll/alonegramgram/AlonegramgramApplication.java
@@ -0,0 +1,13 @@
+package com.ll.alonegramgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlonegramgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlonegramgramApplication.class, args);
+    }
+
+}
